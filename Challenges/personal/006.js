fe6cb33f5f2868942f1e3db3a36528b00d606c04
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-01-29 21:21:14 -0300

    feat:challenges => array indexados. michi app => favourites michis

---
+++ Challenges/personal/006.js
@@ -0,0 +1,52 @@
+/**
+ * Cuando debemos buscar elementos en un array un metodo eficiente es indexar los elementos
+ * 
+ */
+let users = []
+
+for(let i=9999; i>0; i--){
+    users[i] = {
+        id: i,
+        name: Math.random().toString(),
+    } 
+}
+
+const lista = [];
+
+for(let i= 0; i<10000; i++){
+    lista[i] = {
+        id: i,
+        name: Math.random(),
+        created_by: Math.floor(Math.random()*1000),
+    }
+}
+
+// Agregar una propiedad mas llamado created_by_user
+// Como no hacerla
+console.time('usando find')
+
+const conUser = lista.map(elemento => {
+    return{
+        ...elemento,
+        created_by_user: users.find(u => u.id === elemento.created_by) 
+    }
+    
+})
+console.timeEnd('usando find')
+
+/**
+ * Mejor Metodo usando indexacion con reduce
+ */
+const usersIndexado = users.reduce((acc, el) =>{
+    acc[el.id] = el
+    return acc
+}, {}) 
+
+console.time('usando index')
+const conUserIndex = lista.map(elemento => {
+    return {
+        ...elemento,
+        created_by_user: usersIndexado[elemento.created_by]
+    }
+})
+console.timeEnd('usando index')
--- Vanilla/APIREST Fundamentos/003 michi app/app.js
+++ Vanilla/APIREST Fundamentos/003 michi app/app.js
@@ -1,18 +1,52 @@
 const endpoints = {
     API_URL: 'https://api.thecatapi.com/v1/images/search',
+    API_URL_FAVORITE: 'https://api.thecatapi.com/v1/favourites?api_key=live_8E0sutiVRmIiC22KufYVlvStp6zufzqYm4PxfXwwoU04kTungrwDVvegusEAQBeh',
     API_URL_LIMIT: (limit=3) => `https://api.thecatapi.com/v1/images/search?limit=${limit}`,
     API_URL_KEY: (key,limit=3)=> [ 'https://api.thecatapi.com/v1/images/search', `?limit=${limit}`, '&order=Asc', `&api_key=${key}`,].join(''),
 }
-console.log(endpoints.API_URL_LIMIT(5));
-const cards = document.querySelectorAll('.card-cat img')
 
-/* const generateCards = async () => {
-    const result = await (await fetch(CAT_API_URL)).json();
+const cards = document.querySelectorAll('.card-cat img')
+const cardsBtn = document.querySelectorAll('.card-cat button')
+const michifavourites = document.querySelector('.favorite-michi-container')
+
+const generateCards = async () => {
+    const response = await fetch(endpoints.API_URL_LIMIT(2));
+    const data = await response.json();
     for(let i = 0; i < cards.length; i++){
-        cards[i] = result[i].url;
-        console.log(result[i].url)
+        cards[i].src = data[i].url; 
+        cards[i].id = data[i].id;
     }
 }
 
-generateCards(); */
+const getFavouritesMichis = async () => {
+    const response = await fetch(endpoints.API_URL_FAVORITE);
+    const data = await response.json();
+
+    data.map((element) => {
+        if(response.status  === 200){
+            const fav = `<article><img src=${element.image.url} alt='michi'><button>Del Fav</button></article>`;
+            michifavourites.insertAdjacentHTML('beforeend',fav)
+        } 
+    })
+}
+
+cardsBtn.forEach(button => {
+    button.addEventListener('click', async () =>{
+        const image = button.parentNode.querySelector('img')
+        const response = await fetch(endpoints.API_URL_FAVORITE,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify({image_id: image.getAttribute('id')})
+            
+        })
+    })
+}) 
+
+
+document.addEventListener('DOMContentLoaded',() => {
+    generateCards();
+    getFavouritesMichis(); 
+})
 
